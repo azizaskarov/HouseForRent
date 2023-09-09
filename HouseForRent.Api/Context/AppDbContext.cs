6b11d6214e20f93fb8adb5d6b0804198f5e4d272
Azizbek Askarov <azizasqarov773@gmail.com>
2023-09-09 12:29:30 +0500

    Insert guest

---
+++ HouseForRent.Api/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HouseForRent.Api.Entities.Foundation.Guests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HouseForRent.Api.Context;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Guest> Guests => Set<Guest>();
+
+}
---
+++ HouseForRent.Api/Managers/Guests/GuestManager.cs
@@ -0,0 +1,34 @@
+﻿using HouseForRent.Api.Context;
+using HouseForRent.Api.Entities.Foundation.Guests;
+using HouseForRent.Api.Models.Guests;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HouseForRent.Api.Managers.Guests;
+
+public class GuestManager : IGuestManager
+{
+    private readonly AppDbContext dbContext;
+
+    public GuestManager(AppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async ValueTask<Guest> CreateGuestAsync(CreateGuest createGuest)
+    {
+        var guest = new Guest()
+        {
+            FirstName = createGuest.FirstName,
+            LastName = createGuest.LastName,
+            PhoneNumber = createGuest.PhoneNumber,
+            Address = createGuest.Address,
+            BirthDate = createGuest.BirthDate,
+            Email = createGuest.Email,
+            Gender = createGuest.Gender,
+        };
+        
+        EntityEntry<Guest> entityGuest = await dbContext.Guests.AddAsync(guest);
+        await dbContext.SaveChangesAsync();
+        return entityGuest.Entity;
+    }
+}
